5eb448b638260db5a3d2345753ffa7af410c1544
MarziyehKhaleghi <marziyeh.khaleghi92@gmail.com>
2026-04-17 13:42:37 +0200

    Fix CORS syntax error

--- backend/api/main.py
+++ backend/api/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # Tillat React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
